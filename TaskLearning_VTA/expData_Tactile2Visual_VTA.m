1d5d0b30fc97367b744a7b1717ab8da5e6eb5681
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2024-01-14 14:45:59 -0500

    Begin expData setup for VTA cohort 1: M103 & M105

--- TaskLearning_VTA/expData_Tactile2Visual_VTA.m
+++ TaskLearning_VTA/expData_Tactile2Visual_VTA.m
@@ -24,22 +24,32 @@ expData = struct('sub_dir',[],'subjectID',[],'mainMaze',[],...
 
 % Session metadata
 i=1;
-
-expData(i).sub_dir = '230922-m105-maze7-test'; 
-expData(i).subjectID = "mjs20_105";
-expData(i).mainMaze = 7;
-expData(i).npCorrFactor = 0.3;
-
-% i = i+1;
-% expData(i).sub_dir = '230911-m103-maze7'; 
-% expData(i).subjectID = "mjs20_103";
-% expData(i).mainMaze = 7;
-% expData(i).npCorrFactor = 0.3;
-% i = i+1;
-% expData(i).sub_dir = '230911-m105-maze7'; 
+% expData(i).sub_dir = '230922-m105-maze7-test'; 
 % expData(i).subjectID = "mjs20_105";
 % expData(i).mainMaze = 7;
 % expData(i).npCorrFactor = 0.3;
+% i = i+1;
+expData(i).sub_dir = '231006-m105-maze7'; 
+expData(i).subjectID = "mjs20_105";
+expData(i).mainMaze = 7;
+expData(i).npCorrFactor = 0.3;
+i = i+1;
+expData(i).sub_dir = '231009-m105-maze8'; 
+expData(i).subjectID = "mjs20_105";
+expData(i).mainMaze = 8;
+expData(i).npCorrFactor = 0.3;
+i = i+1;
+expData(i).sub_dir = '231017-m105-maze8'; 
+expData(i).subjectID = "mjs20_105";
+expData(i).mainMaze = 8;
+expData(i).npCorrFactor = 0.3;
+i = i+1;
+expData(i).sub_dir = '231101-m105-maze8'; 
+expData(i).subjectID = "mjs20_105";
+expData(i).mainMaze = 8;
+expData(i).npCorrFactor = 0.3;
 
-
-
+% 231006-m105-maze7
+% 231009-m105-maze8
+% 231017-m105-maze8
+% 231101-m105-maze8
