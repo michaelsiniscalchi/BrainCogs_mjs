99106f0c6ff41eda7ccdc7d6fd0de435f701aa33
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2025-05-09 17:04:51 -0400

    minor debug of app, and func to patch previously selected sessions

--- TaskLearning_VTA/test & troubleshooting/patch_roiData.m
+++ TaskLearning_VTA/test & troubleshooting/patch_roiData.m
@@ -1,8 +1,12 @@
-dataDir = 'X:\michael\network-batch\rois';
-searchStr = '-m';
+function [sessionIDs] = patch_roiData(subjID, dataDir)
+
+%Initialize
+sessionIDs = {};
+
+%% Locate ROI data and Time-Projections from all Sessions from Specified Subject
 
 % Search for ROI directories
-temp = dir(fullfile(dataDir,['*' searchStr '*']));
+temp = dir(fullfile(dataDir,['*' subjID '*']));
 for i = 1:numel(temp)
     dirs.sessions{i} = fullfile(dataDir,temp(i).name);
 end
@@ -16,22 +20,51 @@ for i=1:numel(dirs.sessions)
 end
 dirs.roi = dirs.roi(~cellfun(@isempty,dirs.roi));
 
-%%
+%Rename projections to match current App
+fields = {'var_proj','mean_proj','max_proj','filename'};
 for i = 1:numel(dirs.roi)
-    roiFile = fullfile(dirs.roi{i},'roiData.mat');
-    S = load(roiFile);
-    %PATCH: fix variable names for old sessions
-    if isfield(S,'mean_proj')
-        S.mean = S.mean_proj; S=rmfield(S,'mean_proj');
+    S = load(fullfile(dirs.roi{i},'roiData.mat'),...
+        fields{:}, 'mean','var','max','FileName');
+    if any(isfield(S, fields))
+        sessionIDs{i,1} = dirs.roi{i}; %#ok<AGROW>
+        %Replace fields
+        for f = string(fields)
+            if ~isfield(S,f)
+                continue
+            else
+                switch f
+                    case 'mean_proj'
+                        S = replaceField(S, f, 'mean', sessionIDs{i});
+                    case 'var_proj'
+                        S = replaceField(S, f, 'var', sessionIDs{i});
+                    case 'max_proj'
+                        S = replaceField(S, f, 'max', sessionIDs{i});
+                    case 'filename'
+                        S = replaceField(S, f, 'FileName', sessionIDs{i});
+                end
+            end
+        end
+        save(fullfile(dirs.roi{i},'roiData.mat'),'-struct',"S");
     end
-    if isfield(S,'var_proj')
-        S.var = S.var_proj; S=rmfield(S,'var_proj');
+
+    %Remove ROI files with no cell mask
+    fileList = dir(fullfile(dirs.roi{i},'cell*.mat'));
+    for j = 1:numel(fileList)
+        filepath = fullfile(dirs.roi{i},fileList(j).name);
+        S = load(filepath,'bw');
+        if isempty(S.bw)
+            delete(filepath);
+            disp('No ROI found in:');
+            disp(filepath);
+            disp('File deleted.');
+        end
     end
-    if isfield(S,'max_proj')
-        S.max = S.max_proj; S=rmfield(S,'max_proj');
-    end
-    if isfield(S,'filename')
-        S.FileName = S.filename; S=rmfield(S,'filename');
-    end
-   save(roiFile,'-struct',"S");
+
 end
+
+function S = replaceField(S, oldFieldName, newFieldName, sessionID)
+S.(newFieldName) = S.(oldFieldName);
+S = rmfield(S, oldFieldName);
+disp("roiData.mat modified in:")
+disp(sessionID);
+disp([oldFieldName,' replaced with ', newFieldName]);
