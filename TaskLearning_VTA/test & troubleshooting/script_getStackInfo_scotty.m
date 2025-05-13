7c88e8e4ad182004c77fe30acd96232c1a014775
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2025-05-13 12:52:46 -0400

    Create script_getStackInfo_scotty.m

---
+++ TaskLearning_VTA/test & troubleshooting/script_getStackInfo_scotty.m
@@ -0,0 +1,16 @@
+cd('/jukebox/Bezos/michael/_code/General')
+
+search_filter = '250117';
+
+% Set path
+dirs = getRoots();
+addGitRepo(dirs,'General','iCorre-Registration','BrainCogs_mjs','TankMouseVR','U19-pipeline-matlab',...
+    'datajoint-matlab','compareVersions','GHToolbox');
+addpath(genpath(fullfile(dirs.code, 'mym', 'distribution', 'mexa64')));
+
+% Session-specific metadata
+[dirs, expData] = expData_Tactile2Visual_VTA(dirs);
+expData = expData(contains({expData(:).sub_dir}', search_filter)); %Filter by data-directory name, etc.
+
+sessionPath = fullfile(dirs.data, expData.sub_dir);
+stackInfo = getRawStackInfo( sessionPath, [], true );
