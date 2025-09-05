e75955ae2932851292974683e4f0592592ba2265
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2025-09-05 10:58:35 -0400

    script & func to delete results/stackinfo, etc

--- TaskLearning_VTA/test & troubleshooting/delete_resultsFiles.m
+++ TaskLearning_VTA/test & troubleshooting/delete_resultsFiles.m
@@ -1,5 +1,34 @@
+function delete_resultsFiles( search_filter, options )
+
+% Set path
+dirs = getRoots();
+addGitRepo(dirs,'General','iCorre-Registration','BrainCogs_mjs','TankMouseVR','U19-pipeline-matlab',...
+    'datajoint-matlab','compareVersions','GHToolbox');
+addpath(genpath(fullfile(dirs.code, 'mym', 'distribution', 'mexa64')));
+
+% Session-specific metadata
+[dirs, expData] = expData_Tactile2Visual_VTA(dirs);
+expData = expData(contains({expData(:).sub_dir}', search_filter)); %Filter by data-directory name, etc.
+
+% Set parameters for analysis
+[calculate, ~, figures, mat_file, ~] = params_Tactile2Visual_VTA(dirs, expData);
+expData = get_imgPaths(dirs, expData, calculate, figures); %Append additional paths for imaging data if required by 'calculate'
+
 
 for i=1:numel(expData)
+    if exist(mat_file.stack_info(i),"file") && options.stack_info
+        disp(['Deleting ' mat_file.stack_info(i)]);
+    end
+    delete(mat_file.stack_info(i));
+    
+    if exist(mat_file.img_beh(i),"file") && options.img_beh
+        disp(['Deleting ' mat_file.img_beh(i)]);
+    end
     delete(mat_file.img_beh(i));
-     delete(mat_file.results.cellFluo(i));
+
+    if exist(mat_file.results.cellFluo(i),"file") && options.results
+        disp(['Deleting ' mat_file.results.cellFluo(i)]);
+    end
+    delete(mat_file.results.cellFluo(i));
+
 end
---
+++ TaskLearning_VTA/test & troubleshooting/script_delete_resultsFiles.m
@@ -0,0 +1,3 @@
+search_filter = 'm571';
+options = struct('stack_info',true,'img_beh',false,'results',false);
+delete_resultsFiles(search_filter, options);
