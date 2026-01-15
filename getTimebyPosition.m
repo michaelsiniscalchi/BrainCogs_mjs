6e3067eacc709a695276b4e8c498ff2170e7a003
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2026-01-15 17:21:39 -0500

    Update getTimebyPosition.m

--- getTimebyPosition.m
+++ getTimebyPosition.m
@@ -9,9 +9,14 @@ function [time_mat, maxY, iter] = getTimebyPosition(trialData, eventTimes, ySamp
 position = {trialData.position};
 time = cellfun(@(t,st) t+st,{trialData.time},{eventTimes.logStart},'UniformOutput',false);
 
-%Get times corresponding to input sample y-positions
+%Get iterations corresponding to input sample y-positions (in terms of graphical VR feedback)
 maxY            = cellfun(@(pos) cummax(pos(:,2)), position, 'UniformOutput', false); %Take cumulative max
 iter            = accumfun(2, @(x) binarySearch(x, ySample, 1, 1)', maxY);
+
+%Correction for time indexing in ATT runtime code: time from just before iter(i) stored as time(i+1)
+iter = iter + 1; 
+
+%Extract trial-relative times from corresponding iterations
 time_mat        = accumfun(2, @(x) time{x}(iter(:,x)), 1:numel(position));
 
 %Main method taken from sampleViewAngleVsY(), written by Sue Ann Koay
